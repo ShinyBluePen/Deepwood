8ce93db7946b317f1cf564a6d2fed90f2824cd6e
ShinyBluePen <40710541+ShinyBluePen@users.noreply.github.com>
2022-03-28 15:13:17 +0700

    Delete civilization.py

--- civilization.py
+++
@@ -1,9 +0,0 @@
-
-import pygame
-
-from settings import *
-from creature import Creature
-
-class Civilization(Creature):
-    def __init__(self, pos, groups, sprite):
-        super().__init__(pos, groups, sprite)
